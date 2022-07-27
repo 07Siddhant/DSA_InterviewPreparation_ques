50f27ef1f5f78877e741aa47b7d640b274a9b573
07Siddhant <77661983+07Siddhant@users.noreply.github.com>
2022-07-27 22:59:49 +0530

    space_optimised_grid_uniquepaths_dp.cpp
    TC=O(m*n)  SC=O(n)

---
+++ grid_unique_paths_dp.cpp
@@ -0,0 +1,17 @@
+int uniquePaths(int m, int n) {
+    vector<int> prev(n,0);
+    for(int i=0;i<m;i++){
+        vector<int> cur(n,0);
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0)  cur[j]=1;
+            else{
+                int up=0,left=0;
+            if(i>0) up=prev[j];
+            if(j>0)   left=cur[j-1];
+                cur[j]=up+left;
+        }
+    }
+        prev=cur;
+    }
+    return prev[n-1];
+}
