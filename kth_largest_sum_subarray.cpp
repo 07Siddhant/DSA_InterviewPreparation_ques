fc66a8224d7439f7d6094bd1ea87141f92677a65
07Siddhant <77661983+07Siddhant@users.noreply.github.com>
2022-07-29 14:52:05 +0530

    using_heap.cpp

---
+++ kth_largest_sum_subarray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+int getKthLargest(vector<int> &arr, int k)
+{
+     priority_queue<int,vector<int>,greater<int>> pq;
+    int n=arr.size();
+    
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(pq.size()<k){
+                pq.push(sum);
+            }
+            else{
+                if(sum>pq.top()){
+                    pq.pop();
+                    pq.push(sum);
+                }
+            }
+        }
+    }
+    return pq.top();
+}
