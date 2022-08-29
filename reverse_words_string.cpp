9a177871dd4472699fcc3c8ad4047654be0366f1
07Siddhant <77661983+07Siddhant@users.noreply.github.com>
2022-08-29 20:52:15 +0530

    string_words_reverse.cpp

---
+++ reverse_words_string.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        string ans="";
+        string temp="";
+        
+        for(int i=S.length()-1;i>=0;i--){
+            if(S[i]=='.'){
+                reverse(temp.begin(),temp.end());
+                ans=ans+temp;
+                ans.push_back('.');
+                temp="";
+            }
+            else{
+                temp.push_back(S[i]);
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        ans=ans+temp;
+        return ans;
+    } 
+};
